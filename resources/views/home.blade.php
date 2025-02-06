9f821d71c1e6d745e12a1cff7a09debe16e91624
BetrandStevenBukanPeto <110436128+BetrandStevenBukanPeto@users.noreply.github.com>
2025-02-06 15:16:31 +0700

    database litera

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="./dist/output.css" rel="stylesheet">
+</head>
+
+<body class="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+    <img src="img/dummy.png" alt="Logo" class="w-36 mb-4">
+    <h1 class="text-4xl font-bold">LITERA</h1>
+    <h2 class="text-xl text-gray-600">Ini slogan</h2>
+    <a href="page2" class="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">Page2</a>
+</body>
+
+</html>
